9fba18e9ab936364e1c2f9accbc682c07086faad
Ron van der Wijngaard <rvdwijngaard@hotmail.com>
2015-02-16 15:29:43 +0100

    initial version

---
+++ app.js
@@ -0,0 +1,7 @@
+﻿var Customer = require('./lib/index');
+
+var customer = new Customer();
+
+customer.getBalance(function (err, balance) { 
+    console.log("Your balance is " + balance);
+})
---
+++ test/apptest.js
@@ -0,0 +1,18 @@
+﻿var assert = require('assert');
+var Customer = require('../lib/index');
+
+
+describe('Test Suite 1', function () {
+    customer = new Customer("piet");
+    it('Open Balance', function(done) {
+        customer.getBalance(function (err, balance) { 
+            assert.equal(balance, 2500, "the balance is ok");
+            done();
+        });
+    })
+
+    it('Test 2', function() {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    })
+})
