43094f263d6a18f1b0dd486b91db604e14220009
Ron van der Wijngaard <rvdwijngaard@hotmail.com>
2015-02-19 13:50:26 +0100

    add missing file

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['TI logging POC'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '5492c97c4a6b704d6447b9e2580b1005955f6a2a',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
